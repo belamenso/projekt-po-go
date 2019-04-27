35f1443f03ccfd12529a3e737d1904f7b9d2801f
Bartosz Białas <belamenso@gmail.com>
2019-04-27 12:49:41 +0200

    Dokumentacja dla klasy Stone

--- po-go/src/go/Stone.java
+++ po-go/src/go/Stone.java
@@ -1,10 +1,15 @@
 package go;
 
+/**
+ * Klasa reprezentująca kolor kamienia. Nie ma kamieni pustych, gra reprezentuje je jako Optional.empty()
+ */
 public enum Stone {
     White, Black;
 
+    /**
+     * Wskazuje na kolor kamienia przeciwnika
+     */
     public Stone opposite;
-
     static {
         White.opposite = Black;
         Black.opposite = White;
