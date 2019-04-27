ba1d6db518423bd4b93098d8daa83c6937f15c8b
Bartosz Białas <belamenso@gmail.com>
2019-04-27 13:05:17 +0200

    Dokumentacja dla klasy GameLogic

--- po-go/src/go/GameLogic.java
+++ po-go/src/go/GameLogic.java
@@ -2,15 +2,21 @@ package go;
 
 import util.Pair;
 
-import javax.swing.text.html.Option;
 import java.util.Queue;
 import java.util.Optional;
 import java.util.ArrayList;
 import java.util.LinkedList;
 
+/**
+ * Singleron realizujący podstawowe operacje logiki gdy na obiektach typu Board
+ * Napisany funkcyjnie, niczego (trwale) nie modyfikuje
+ */
 public enum GameLogic {
     gameLogic;
 
+    /**
+     * Przesunięcia indeksów do sąsiadów na lewo, prawo, w górę i dół
+     */
     private final ArrayList<Pair<Integer, Integer>> offsets;
     {
         offsets = new ArrayList<>(4);
@@ -20,19 +26,37 @@ public enum GameLogic {
         offsets.add(new Pair<>(1,0));
     }
 
+    /**
+     * Czy podana pozycja mieści się w plaszy z której rzekomo pochodzi?
+     * TODO przenieść to do klasy Board
+     */
     private boolean indicesOk(Board board, int i, int j) {
         return i >= 0 && i < board.getSize() && j >= 0 && j < board.getSize();
     }
 
+    /**
+     * Tworzy świerzą macierz boolowską wielkości planszy podanej jako argument
+     */
     private boolean[][] boolMatrix(Board board) {
         return new boolean[board.getSize()][board.getSize()];
     }
 
+    /**
+     * Reprezentuje maksymalną spójną grupę kamieni jednego koloru na planszy
+     * liberties to liczba pustych przecięć, które bezpośrednio sąsiadują z jakimś zewnętrznym kamieniem grupy
+     */
     private class Group {
         ArrayList<Pair<Integer, Integer>> group = new ArrayList<>();
         int liberties = 0;
     }
 
+    /**
+     * Algorytm BFS rozpoznawania grupy z kamieniem w miejscu (i, j)
+     * @param seen macierz boolowska w której zaznacza się, czy dany kamien (nie puste przecięcie!) był już widziany,
+                   jeśli prowadzi się przegląd całej planszy, należy zachowywać macierz pomiędzy wywołaniami, jeśli nie,
+                   należy podawać ją świerzą za każdym razem
+     * @return Rozpoznana grupa kamieni
+     */
     private Group collectGroup(Board board, int i, int j, boolean[][] seen) {
         Group ret = new Group();
         boolean[][] libertiesSeen = boolMatrix(board);
@@ -67,11 +91,22 @@ public enum GameLogic {
         return ret;
     }
 
+    /**
+     * Reprezentuje spójne terytorium <b>pustych przecięć</b>
+     * captor - gracz do kótrego należy terytorium lub Optional.empty() jeśli nie należy do żadnego
+     */
     class Territory {
         ArrayList<Pair<Integer, Integer>> territory = new ArrayList<>();
         Optional<Stone> captor = Optional.empty();
     }
 
+    /**
+     * Algorytm BFS znajdowania maksymalnego spójnego terytorium do którego należy puste przecięcie (i, j)
+     * TODO chwilowo to nie obejmuje usuwania dead stones pod koniec gry, czyli nie jest zgodne z zasadami
+     * @param seen macierz boolowska w której zaznacza się, czy dane <b>puste przecięcie</b> było już odwiedzone
+                   jeśli prowadzi się przegląd całej planszy, należy zachowywać macierz pomiędzy wywołaniami, jeśli nie,
+                   należy podawać ją świerzą za każdym razem
+     */
     private Territory collectTerritory(Board board, int i, int j, boolean[][] seen) {
         assert indicesOk(board, i, j) && board.get(i, j).isEmpty(); // TODO
         assert !seen[i][j];
@@ -109,6 +144,14 @@ public enum GameLogic {
         return ret;
     }
 
+    /**
+     * determinuje, czy ruch gracza color na miejsce (i, j) jest dozwolony, to znaczy czy
+     * <ol>
+     * <li>indeks jest poprawny</li>
+     * <li>nie ma tam innego kamienia</li>
+     * <li>ruch nie jest samobójczy albo jest samobójczy, ale natychmiast przerywa zagrożenie</li>
+     * </ol>
+     */
     public boolean movePossible(Board board, int i, int j, Stone color) {
         if (!indicesOk(board, i, j)) return false;
         if (board.get(i, j).isPresent()) return false;
@@ -139,6 +182,10 @@ public enum GameLogic {
         }
     }
 
+    /**
+     * @return Zwrava listę pól, na których znajdują się kamienie przeciwnika, które zostały właśnie przejęte
+     * @param colorJustPlaced gracz, który właśnie dokonał ruchu
+     */
     public ArrayList<Pair<Integer, Integer>> captured(Board board, Stone colorJustPlaced) {
         ArrayList<Pair<Integer, Integer>> ret = new ArrayList<>();
         boolean[][] seen = boolMatrix(board);
@@ -154,6 +201,10 @@ public enum GameLogic {
         return ret;
     }
 
+    /**
+     * @return Zwraca listę przejętych (<b>czyli z dobrze zdefiniowanym captorem</b>) terytoriów jakie powstały na planszy
+     * TODO to nie usuwa martwych kamieni z planszy, czyli technicznie jest niezgodne z zasadami gry
+     */
     public ArrayList<Territory> capturedTerritories(Board board) {
         ArrayList<Territory> ret = new ArrayList<>();
         boolean[][] seen = boolMatrix(board);
