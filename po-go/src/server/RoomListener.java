d323067d588d04c361ab17ddcdf5ea4d6a2c8972
Bartosz Białas <belamenso@gmail.com>
2019-04-27 20:27:00 +0200

    Dodanie stanu gry do RoomListener

--- po-go/src/server/RoomListener.java
+++ po-go/src/server/RoomListener.java
@@ -1,5 +1,6 @@
 package server;
 
+import go.Board;
 import go.GameplayManager;
 import go.ReasonMoveImpossible;
 import go.Stone;
@@ -48,6 +49,27 @@ public class RoomListener implements ServerListener {
      * niekompatybilne pokoje nie powinny być widoczne dla klienta
      */
     public String gameEngineVersion() { return manager.getEngineVersion(); }
+
+    public enum RoomState {
+        GameInProgress,
+        GameInterrupted,
+        GameFinished,
+        EmptyRoom,
+        WaitingForBlack, // TODO późniejsza wersja może pozwolić na wybór koloru
+        WaitingForWhite
+    }
+
+    public RoomState getRoomState() {
+        if (manager.interrupted()) return RoomState.GameInterrupted;
+        if (manager.finished()) return RoomState.GameFinished;
+        if (manager.inProgress()) return RoomState.GameInProgress;
+        if (clients.size() == 0) return RoomState.EmptyRoom;
+        if (clients.size() == 1) return RoomState.WaitingForWhite; // TODO wybór koloru
+        else {
+            assert false;
+            return RoomState.EmptyRoom; // z jakiegoś powodu Java nie pozwala na kończenie metody assert false
+        }
+    }
 
     /**
      * @return TODO Późniejsza wersja będzie to implementowała
