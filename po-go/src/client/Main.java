5e54f60b275ccca361a79c250af20ce34c86d5a0
Bartosz Białas <belamenso@gmail.com>
2019-04-28 11:04:21 +0200

    Chwilowo usunąłem setResizable(false) (u mnie okno b. małe)

--- po-go/src/client/Main.java
+++ po-go/src/client/Main.java
@@ -4,7 +4,6 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -28,6 +27,4 @@ public class Main extends Application {
         window.setScene(scene);
         window.show();
     }
-
-
 }
